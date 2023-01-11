92876b98605b8bcf8d99c39cd3421c5c3e3e4202
Renato Farias <santosrenato351@gmail.com>
2023-01-11 13:17:54 -0300

    user insert

--- src/main/java/com/educandoweb/projetospring/resources/UserResource.java
+++ src/main/java/com/educandoweb/projetospring/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.educandoweb.projetospring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,12 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id)  {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
